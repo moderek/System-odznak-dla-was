dcf9aedd882996085e82bd1ece7c69bbd7be9016
Adrian Puczyłowski <49859031+moderek@users.noreply.github.com>
2019-10-07 20:15:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(".js-switch").click(function() {
+	$(".main-content").toggleClass("as-card");
+});
+
+
+var reader = new FileReader();
+reader.onload = function(e) {
+	$("#imager").attr("src", e.target.result);
+};
+
+function readURL(input) {
+	if (input.files && input.files[0]) {
+		$("#imager").css("visibility",'visible');
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+
+$("#image-input").change(function() {
+	readURL(this);
+});
